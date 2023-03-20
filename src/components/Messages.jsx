80b717146023890ceb327421ef7aa5bc68946f07
Gyanendra Tiwari <mrgyan432@gmail.com>
2023-03-21 03:03:26 +0530

    initial commit

---
+++ src/components/Messages.jsx
@@ -0,0 +1,31 @@
+import { doc, onSnapshot } from 'firebase/firestore';
+import React, { useContext, useEffect, useState } from 'react'
+import { ChatContext } from '../context/ChatContext';
+import { db } from '../firebase';
+import Message from './Message'
+
+const Messages = () => {
+  const { data } = useContext(ChatContext);
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const unsub = onSnapshot(doc(db, "chats", data.chatId), (doc) => {
+      doc.exists() && setMessages(doc.data().messages);
+    })
+
+    return () => {
+      unsub();
+    }
+
+  },[data.chatId]);
+
+  return (
+    <div className='messages'>
+        {messages.map((m) => (
+          <Message key={m.id} message={m} />
+        ))}
+    </div>
+  )
+}
+
+export default Messages
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions needed from the SDKs needed
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// Web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDU-t1KXJTATzpm_icmH4EGT3LgMNeBTYE",
+  authDomain: "baatchit-noobmaster432.firebaseapp.com",
+  projectId: "baatchit-noobmaster432",
+  storageBucket: "baatchit-noobmaster432.appspot.com",
+  messagingSenderId: "139988169705",
+  appId: "1:139988169705:web:bf1f5bcb2f1f6cd5811178",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const storage = getStorage();
+export const db = getFirestore();
